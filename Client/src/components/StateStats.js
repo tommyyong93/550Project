ce1f341fd6ee385cf6ab26fb3c84fac35244127a
Ellen Gao <ellengao@Ellens-MBP.home>
2020-11-28 12:46:41 -0500

    tooltip box

--- Client/src/components/StateStats.js
+++ Client/src/components/StateStats.js
@@ -203,9 +203,10 @@ export default class StateStats extends React.Component {
           <Card className='state-results-container'>{this.state.selectedState ? `Here are the top ${this.state.topResults.length} nursing homes in ${this.state.stateFullName}` : "Click on a state to learn more!"}
             {this.state.topResults}
             {this.state.selectedState ?
-              <Tooltip className={Classes.TOOLTIP_INDICATOR} position={Position.RIGHT} content={"We ranked the best nursing homes in the state by selecting the following metrics and assigning them arbitrary weights. The nursing homes are then ranked based on the weighted average of these metrics: (OverallRating, HealthInspRating, StaffRating, QMRating, AverageHrsPerResPerDay_OverallPercentile, ReportedIncidents, Complaints, TotalCovidDeaths_OverallPercentile, and VentilatorsInFacility_OverallPercentile)"}>
+              <Tooltip className={Classes.TOOLTIP_INDICATOR} position={Position.RIGHT} content={
+                "We ranked the best nursing homes in the state by selecting the following metrics and assigning them arbitrary weights. The nursing homes are then ranked based on the weighted average of these metrics: (OverallRating, HealthInspRating, StaffRating, QMRating, AverageHrsPerResPerDay_OverallPercentile, ReportedIncidents, Complaints, TotalCovidDeaths_OverallPercentile, and VentilatorsInFacility_OverallPercentile)"}>
                 How did we rank these nursing homes? 
-              </Tooltip> : <></>}
+              </Tooltip> : <></>} 
           </Card>
           <div className="map-container">
             <div className='country-row'>
